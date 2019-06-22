b2878d77c00f860a9869672243c05ed700d7cd71
Arianna Kosten <arianna.kosten@gmail.com>
2019-06-22 07:15:20 +0000

    Automatically backed up by Learn

--- db/migrate/20190619063932_create_dogs.rb
+++ db/migrate/20190619063932_create_dogs.rb
@@ -9,7 +9,11 @@ class CreateDogs < ActiveRecord::Migration[5.2]
 
 # def up
 #     create_table :dogs do |t|
+<<<<<<< HEAD
 #       t.string :namelearn
+=======
+#       t.string :name
+>>>>>>> ea8b88f3b78fcf623b448a8a7306dc46f8a3836a
 #       t.string :breed
 #     end
 #   end
